a80fd197c1d301f23b66d58110bb270d95d5cfdf
Javier Arambula Rascon <ja1473@Javiers-MacBook-Pro.local>
2025-07-14 12:34:16 -0400

    polished up quite a bit for future use, could be nice to have

--- scripts/underscore_namer.sh
+++ scripts/underscore_namer.sh
@@ -1,28 +1,67 @@
 #!/bin/bash
+# Renames files in DIR by replacing spaces with underscores and moves them to OUT.
 
 DIR=$1
 OUT=$2
+MODE=$3
+
+# Removes first trailing slash if present
+DIR="${DIR%/}"
+OUT="${OUT%/}"
+
+# Main function for renaming files
+function rename() {
+    for file in "$DIR"/*; do
+        if [ -f "$file" ]; then
+            # Extracts the basename and removes spaces
+            base_name=$(basename "$file")
+            new_file="${base_name// /_}"
+
+            echo "Moving $base_name to $OUT/$new_file..."
+            if mv "$file" "$OUT/$new_file"; then
+                echo "Success."
+            else
+                echo "Failed moving $new_file."
+            fi
+        else
+            echo "$file is not a file."
+        fi
+    done
+    echo "Done renaming $DIR"
+}
+
+# Dry run function to see example output
+function dry() {
+    for file in "$DIR"/*; do
+        if [ -f "$file" ]; then
+            base_name=$(basename "$file")
+            new_file="${base_name// /_}"
+
+            echo "[Dry run] Would move $base_name to $OUT/$new_file"
+        else
+            echo "$file is not a file."
+        fi
+    done
+    echo "Dry run complete for $DIR"
+}
 
 if [ ! -d "$DIR" ] || [ ! -d "$OUT" ]; then
-    echo "Usage: $0 input/dir/ output/dir/"
+    echo "Usage: $0 input/dir/ output/dir/ --run"
+    echo "Optional: --dry for a dry run"
     exit 1
 fi
 
-for file in $DIR/*; do
-    if [ -f "$file" ]; then
-        base_name=$(basename "$file")
-        new_file="${base_name// /_}"
+# Create output directory if it does not exist
+mkdir -p "$OUT"
 
-        echo "Moving $new_file to $OUT..."
-        mv $DIR/"$base_name" "$OUT/$new_file"
+if [[ "$MODE" == "--dry" ]]; then
+    dry
+elif [[ "$MODE" == "--run" ]]; then
+    rename
+else
+    echo "Unexpected argument: $MODE"
+    echo "Try --dry or --run instead"
+    exit 1
+fi
 
-        if [ $? -ne 0 ]; then
-            echo "Failed moving $new_file."
-        else
-            echo "Success."
-        fi
-    else
-        echo "$file is not a file."
-    fi
-echo "Done renaming $DIR"
-done
+exit 0
